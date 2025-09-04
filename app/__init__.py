b6b2d66835a9994ddb90433b7e4cd4859c37103b
Glebious <sprpmail@gmail.com>
2025-09-04 08:52:24 +0300

    подключение блюпринт

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,30 @@
 import os
 from flask import Flask
 from .extensions import db, migrate, login_manager
+from .auth import auth_bp
 
 def create_app():
-    app = Flask(__name__, template_folder="../templates", static_folder="../static")
+    app = Flask(
+        __name__,
+        template_folder="../templates",
+        static_folder="../static"
+    )
     app.config.from_object("config.Config")
 
     # init extensions
     db.init_app(app)
     migrate.init_app(app, db)
+
     login_manager.init_app(app)
-    login_manager.login_view = "login"  # без main, просто login
+    login_manager.login_view = "auth.login"  # указываем с префиксом blueprint
 
     # импорт моделей (чтобы Alembic их видел)
-    from app import models
+    from app import models  # noqa
 
-    # роуты прямо тут
+    # регистрация блюпринтов
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    # тестовые роуты
     @app.route("/")
     def index():
         return "Привет! Это главная страница."
@@ -25,4 +34,3 @@ def create_app():
         return {"status": "ok"}
 
     return app
-
