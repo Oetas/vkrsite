fb9900d3dbd369bb04563db389f714d2a19a32e4
Glebious <sprpmail@gmail.com>
2025-09-07 16:04:19 +0300

    Добавление админ фомр

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SelectField, BooleanField
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=80)])
@@ -29,4 +30,19 @@ class FileUploadForm(FlaskForm):
         FileAllowed(["pdf", "docx", "xlsx"], "Только pdf/docx/xlsx")
     ])
     submit = SubmitField("Загрузить")
+
+class AdminUserForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(max=255)])
+    username = StringField("Username", validators=[DataRequired(), Length(max=80)])
+    is_active = BooleanField("Active")
+    roles = SelectField("Role", coerce=int)  # будем заполнять choices динамически
+    submit = SubmitField("Сохранить")
+
+class CourseForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(max=255)])
+    slug = StringField("Slug", validators=[DataRequired(), Length(max=255)])
+    description = TextAreaField("Description")
+    level = SelectField("Level", choices=[("beginner","beginner"),("intermediate","intermediate"),("advanced","advanced")])
+    is_published = BooleanField("Published")
+    submit = SubmitField("Save")
 
