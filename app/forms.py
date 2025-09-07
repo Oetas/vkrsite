41060196bbc9bff7beb84be7f61c48f8fc81cbb3
Glebious <sprpmail@gmail.com>
2025-09-07 13:06:12 +0300

    Добавлена FileUploadForm для загрузки файлов

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=80)])
@@ -21,3 +22,11 @@ class ContactForm(FlaskForm):
     subject = StringField("Тема", validators=[Length(max=255)])
     message = TextAreaField("Сообщение", validators=[DataRequired(), Length(min=5)])
     submit = SubmitField("Отправить")
+
+class FileUploadForm(FlaskForm):
+    file = FileField("Файл (PDF/DOCX/XLSX)", validators=[
+        FileRequired(),
+        FileAllowed(["pdf", "docx", "xlsx"], "Только pdf/docx/xlsx")
+    ])
+    submit = SubmitField("Загрузить")
+
